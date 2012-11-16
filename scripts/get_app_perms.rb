addeda92fbac17df36e6e28e232a867f9948fd68
Max <maxfeldman14@gmail.com>
2012-11-16 03:25:38 -0800

    Finished scrape and handle app

--- scripts/get_app_perms.rb
+++ scripts/get_app_perms.rb
@@ -3,6 +3,7 @@
 # GATHER THE APP PERMISSIONS!
 
 require 'mechanize'
+# require 'debugger' ; Debugger.start(post_mortem: true)
 
 class PlayScraper
   def initialize
@@ -10,33 +11,56 @@ class PlayScraper
     @agent = Mechanize.new
   end
 
-  def handle_app app_url
+  def handle_app app_link
     # Fetch the permissions for the app at the given url
+    app = {}
+    page = app_link.click
+    # Class = doc-banner-title identifies the title
+    # id parameter is the full name of the application
+    # doc-permission-description and doc-permission-description-full can be
+    # grabbed pairwise to indicate which permissions the app has.
+    app[:title] = page.search('h1.doc-banner-title').first.content
+    app[:name] = Hash[URI.decode_www_form(app_link.uri.query)]['id']
+
+    perm_names = page.search('div.doc-permission-description').collect do |element|
+      element.content
+    end
+    perm_descriptions = page.search('div.doc-permission-description-full').collect do |element|
+      element.content
+    end
     
+    app[:permissions] = Hash[perm_names.zip(perm_descriptions)]
+    return app
   end
 
   def scrape(query, free=false)
+    # debugger
     url = URI("https://play.google.com/store/search")
     # Params: q=<query>, price=1 (if free), start=multiples of 48, num=48
-    params = {q: query, num: 48}
+    params = {q: query, num: 48, c: "apps"}
     params[:price] = 1 if free
-    catch :done do
-      loop do |i|
-        # Iterate through all apps from google play corresponding to the query,
-        # then call handle_app on each app.
-        
-        # Construct url with parameters.
-        params[:start] = i * params[:num] 
-        url.query = URI.encode_www_form(params)
+    i = 0
+    loop do
+      # Iterate through all apps from google play corresponding to the query,
+      # then call handle_app on each app.
+      
+      # Construct url with parameters.
+      params[:start] = i * params[:num] 
+      url.query = URI.encode_www_form(params)
 
-        # Open url, call handle_app on each link to an app.
-        page = @agent.get(url)
-        page.links_with(class: "title").each do |link|
-          handle_app link
-        end
-
+      # Open url, call handle_app on each link to an app.
+      page = @agent.get(url)
+      page.links_with(class: "title").each do |link|
+        @apps << handle_app(link)
       end
+      i += 1
     end
+
+  rescue Mechanize::ResponseCodeError => e
+    $stderr.puts e
+  ensure
+    puts @apps.inspect
+  
   end
 
 end
